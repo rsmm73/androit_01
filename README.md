5b8b0a8d580af71c65dd610c6f803e5b2f431529
curso <rsmm73@hotmail.com>
2024-09-03 14:13:01 +0200

    login

--- README.md
+++ README.md
@@ -24,3 +24,4 @@ del curso 2023-2024.
 
 **Ejercicios**
 - [Ejercicio 1](docs/ut02/ut2-ej1.pdf)
+- [Ejercicio 2](docs/ut02/ut2-ej2.pdf)
---
+++ app/src/main/java/com/example/androit_01/features/login/presentation/LoginActivity.kt
@@ -0,0 +1,25 @@
+package com.example.androit_01.features.login.presentation
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.androit_01.R
+import com.example.androit_01.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+
+}
